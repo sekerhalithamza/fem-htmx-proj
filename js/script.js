b44aed8f0b40199279047e8645c6ff2a036fbedf
sekerhalithamza_dell <shalithamza@gmail.com>
2024-07-08 00:32:54 +0300

    batman

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", (event) => {
+	document.body.addEventListener("htmx:beforeSwap", function(evt) {
+		if (evt.detail.xhr.status === 422) {
+			evt.detail.shouldSwap = true;
+			evt.detail.isError = false;
+		}
+	});
+});
